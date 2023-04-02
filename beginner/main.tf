e95036cf4b2f59b21f7a736d74ad76b93590efdb
kuropenguin <kuro1994.t@gmail.com>
2023-04-02 20:33:57 +0900

    add beginner ec2

--- beginner/main.tf
+++ beginner/main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  profile = "terraform" // aws-cli に登録したアカウント
-  region  = "aws-northeast-1"
+  // aws-cli に登録したアカウント
+  profile = "terraform"
+
+  region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "hello-world" {
   ami           = "ami-06fdbb60c8e83aa5e"
   instance_type = "t2.micro"
 }
