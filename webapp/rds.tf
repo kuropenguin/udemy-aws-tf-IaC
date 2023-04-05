2add67b6d093de3d5308ea3b3c44b68f0c6fca63
kuropenguin <kuro1994.t@gmail.com>
2023-04-05 20:21:13 +0900

    add option group

--- webapp/rds.tf
+++ webapp/rds.tf
@@ -11,5 +11,10 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
     name  = "character_set_database"
     value = "utf8mb4"
   }
+}
 
+resource "aws_db_option_group" "mysql_standalone_optiongroup" {
+  name                 = "${var.project}-${var.environment}-mysql-standalone-optiongroup"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
 }
