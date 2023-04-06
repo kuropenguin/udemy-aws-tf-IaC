26fa44ed46890958a7a3f752a1134c1d6697edf8
kuropenguin <kuro1994.t@gmail.com>
2023-04-06 20:43:17 +0900

    fix random

--- webapp/rds.tf
+++ webapp/rds.tf
@@ -32,3 +32,31 @@ resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
     Env     = var.environment
   }
 }
+
+resource "random_string" "db_password" {
+  length  = 16
+  special = false
+}
+
+
+resource "aws_db_instance" "mysql_standalone" {
+  engine         = "mysql"
+  engine_version = "8.0.20"
+
+  identifier = "${var.project}-${var.environment}-mysql-standalone"
+
+  username = "admin"
+  password = random_string.db_password.result
+
+  instance_class = "db.t2.micro"
+
+  allocated_storage     = 20
+  max_allocated_storage = 50
+  storage_type          = "gp2"
+  storage_encrypted     = false
+
+  mulit_az               = false
+  availability_zone      = "ap-northeast-1a"
+  db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
