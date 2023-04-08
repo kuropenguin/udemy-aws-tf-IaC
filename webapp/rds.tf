e1b4c571812c2454b32ff63488a5966d59e61a28
kuropenguin <kuro1994.t@gmail.com>
2023-04-08 13:35:18 +0900

    add rds

--- webapp/rds.tf
+++ webapp/rds.tf
@@ -41,7 +41,7 @@ resource "random_string" "db_password" {
 
 resource "aws_db_instance" "mysql_standalone" {
   engine         = "mysql"
-  engine_version = "8.0.20"
+  engine_version = "8.0.28"
 
   identifier = "${var.project}-${var.environment}-mysql-standalone"
 
@@ -55,8 +55,30 @@ resource "aws_db_instance" "mysql_standalone" {
   storage_type          = "gp2"
   storage_encrypted     = false
 
-  mulit_az               = false
+  multi_az               = false
   availability_zone      = "ap-northeast-1a"
   db_subnet_group_name   = aws_db_subnet_group.mysql_standalone_subnetgroup.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  publicly_accessible    = false
+  port                   = 3306
+
+  name                 = "tastylog"
+  parameter_group_name = aws_db_parameter_group.mysql_standalone_parametergroup.name
+  option_group_name    = aws_db_option_group.mysql_standalone_optiongroup.name
+
+  backup_window              = "04:00-05:00"
+  backup_retention_period    = 7
+  maintenance_window         = "Mon:05:00-Mon:08:00"
+  auto_minor_version_upgrade = false
+
+  deletion_protection = false
+  skip_final_snapshot = true
+
+  apply_immediately = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone"
+    Project = var.project
+    Env     = var.environment
+  }
 }
